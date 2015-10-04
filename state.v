2e5e3b558bf911237385ae3f0581aaec939459bb
Aleksandar Kuktin <akuktin@gmail.com>
2015-10-05 00:05:12 +0200

    [ddr] Removed unnedded pins in state, added enter_state.

--- state.v
+++ state.v
@@ -4,16 +4,13 @@ module states(input CLK,
 	      input [2:0]      COMMAND,
 	      output 	       CHANGE_POSSIBLE,
 	      output reg [2:0] STATE,
-	      output 	       READ_ALLOWED,
-	      output 	       WRITE_ALLOWED,
 	      output reg       CLOCK_COMMAND);
   reg [1:0] 	     counter;
   reg 		     state_is_readwrite;
 
-  assign CHANGE_POSSIBLE = (counter == 2'h3) ? 1 : 0;
-
-  assign READ_ALLOWED = CHANGE_POSSIBLE | (STATE == `READ);
-  assign WRITE_ALLOWED = CHANGE_POSSIBLE | (STATE == `WRTE);
+  assign CHANGE_POSSIBLE = ((counter == 2'h3) ||
+			    (state_is_readwrite && (COMMAND == STATE))) ?
+			   1 : 0;
 
   always @(posedge CLK)
     if (!RST)
@@ -53,3 +50,62 @@ module states(input CLK,
 	end
 
 endmodule // states
+
+module enter_state(input CLK,
+		   input 	    RST,
+		   input [11:0]     PAGE_REQUEST,
+		   input 	    WE,
+		   input 	    DO_ACT,
+		   input 	    CHANGE_POSSIBLE,
+		   input 	    CLOCK_COMMAND,
+		   output reg [2:0] COMMAND,
+		   output 	    CHANGE_REQUESTED);
+  reg [11:0] 			    page_current;
+  reg [2:0] 			    command_sequence[2:0];
+  reg [1:0] 			    command_len;
+
+  wire [2:0] 			    rw_command;
+
+  assign CHANGE_REQUESTED = (command_len == 2'h3) ? 0 : 1;
+  assign rw_command = WE ? `WRTE : `READ;
+
+  always @(posedge CLK)
+    if (!RST)
+      begin
+	page_current <= 0; /* TODO: will have to be hacked */
+	command_sequence <= {`NOP/*actually PRCH*/,`ACTV,`READ};
+	command_len <= 2'h3;
+	COMMAND <= `NOP;
+      end
+    else
+      begin
+	if (CHANGE_REQUESTED)
+	  begin
+	    if (CHANGE_POSSIBLE)
+	      begin
+		command_len <= command_len +1;
+		COMMAND <= command_sequence[command_len];
+		page_current <= PAGE_REQUEST;
+		/* TODO: setup data receivers/senders here */
+	      end
+	    else
+	      if (CLOCK_COMMAND)
+		COMMAND <= `PRCH;
+	      else
+		COMMAND <= `NOP;
+	  end // if (CHANGE_REQUESTED)
+	else
+	  begin
+	    command_sequence <= {`NOP/*actually PRCH*/,`ACTV,rw_command};
+
+	    if (DO_ACT)
+	      begin
+		if (PAGE_REQUEST == page_current)
+		  command_len <= 2'h2;
+		else
+		  command_len <= 2'h0;
+	      end
+	  end // else: !if(CHANGE_REQUESTED)
+      end
+
+endmodule // enter_state
