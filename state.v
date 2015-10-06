ccbbabdb29370e2ef72ca865ddd7b9eb53eabafd
Aleksandar Kuktin <akuktin@gmail.com>
2015-10-06 23:56:10 +0200

    [ddr] Probably wrote DQ/DQS read/write.

--- state.v
+++ state.v
@@ -122,49 +122,45 @@ module outputs(input CLK_p,
 	       input 		 CHANGE_POSSIBLE,
 	       input [31:0] 	 DATA_W,
 	       input 		 WE,
-	       inout [31:0] 	 DQ,
+	       inout [15:0] 	 DQ,
 	       inout 		 DQS,
 	       output reg [31:0] DATA_R,
 	       output reg 	 DATA_VALID);
-  reg [31:0] 			 dq_driver, dq_driver_pre;
+  reg [15:0] 			 dq_driver;
+  reg [31:0] 			 dq_driver_pre, dq_driver_holdlong;
   reg 				 dqs_driver, pipe_clk_dqs;
-  reg 				 do_write, will_write;
-  reg 				 will_read, really_will_read, do_read;
+  reg 				 will_write, do_write, do_deltawrite, do_halfwrite;
+  reg 				 will_read, really_will_read, about_to_read,
+				 do_read, reading;
 
-  /* Missing: the actual dual-pump action. */
+  assign DQ = do_deltawrite ? dq_driver : {{16}1'bz};
+  assign DQS = (do_write | do_halfwrite) ? dqs_driver : 1'bz;
 
-  assign DQ = do_write ? dq_driver : {{32}1'bz};
-  assign DQS = (pipe_clk_dqs) ? dqs_driver : 1'bz;
-
-  always @(CLK_p)
-    dqs_driver <= CLK_p;
+  always @(CLK_n)
+    dqs_driver <= CLK_n;
 
   always @(posedge CLK_n)
     if (!RST)
       begin
-	pipe_clk_dqs <= 0;
-      end
-    else
-      begin
-	if (do_write | will_write)
-	  pipe_clk_dqs <= 1;
-	else
-	  pipe_clk_dqs <= 0;
-      end
-
-  always @(posedge CLK_p)
-    if (!RST)
-      begin
+	dq_driver_pre <= 0;
+	will_read <= 0;
+	will_write <= 0;
+	do_read <= 0;
+	about_to_read <= 0;
+	really_will_read <= 0;
+	do_write <= 0;
+	dq_driver_holdlong <= 0;
       end
     else
       begin
 	will_write <= 0;
 	do_write <= will_write;
-	dq_driver <= dq_driver_pre;
+	dq_driver_holdlong <= dq_driver_pre;
 
 	will_read <= 0;
 	really_will_read <= will_read;
-	DATA_VALID <= really_will_read;
+	about_to_read <= really_will_read;
+	do_read <= about_to_read;
 
 	if (CHANGE_REQUESTED & CHANGE_POSSIBLE)
 	  begin
@@ -179,7 +175,41 @@ module outputs(input CLK_p,
 	  end // if (CHANGE_REQUESTED & CHANGE_POSSIBLE)
       end // else: !if(!RST)
 
-  always @(posedge CLK_d)
-    DATA_R <= DQ;
+  always @(posedge CLK_p)
+    if (!RST)
+      begin
+	do_halfwrite <= 0;
+	reading <= 0;
+      end
+    else
+      begin
+	do_halfwrite <= do_write;
+	reading <= do_read;
+      end
+
+  always @(CLK_d)
+    if (!RST)
+      begin
+	do_deltawrite <= 0;
+	dq_driver <= 0;
+	DATA_R <= 0;
+	DATA_VALID <= 0;
+      end
+    else
+      begin
+	do_deltawrite <= do_write;
+
+	if (dqs_driver)
+	  begin
+	    dq_driver <= dq_driver_holdlong[15:0];
+	    DATA_R[31:16] <= DQ;
+	  end
+	else
+	  begin
+	    dq_driver <= dq_driver_holdlong[31:16];
+	    DATA_R[15:0] <= DQ;
+	    DATA_VALID <= reading;
+	  end
+      end
 
 endmodule // outputs
