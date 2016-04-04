bdba02da81259f4d2896c9a7b4fbcbc7ce83d321
Aleksandar Kuktin <akuktin@gmail.com>
2016-04-04 21:34:20 +0200

    [aexm] Add an auxiliary module for enabling/disabling cache activity.

---
+++ aexm/aexm_aux.v
@@ -0,0 +1,18 @@
+module cache_stall(input  cache_busy,
+		   input  cpu_force,
+		   output cache_enable);
+  reg 			  xcache_enable;
+  assign cache_enable = xcache_enable;
+
+  always @(cache_busy or cpu_force)
+    begin
+      if (cpu_force)
+	xcache_enable <= 1;
+      else
+	if (cache_busy)
+	  xcache_enable <= 1;
+	else
+	  xcache_enable <= 0;
+    end
+
+endmodule // cache_stall
