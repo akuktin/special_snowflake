ed83caec08334f045ca025d236172255dad0c51b
Aleksandar Kuktin <akuktin@gmail.com>
2016-04-04 21:35:58 +0200

    [aexm] Clarify a signals nature in aexm_aux/cache_stall.

--- aexm/aexm_aux.v
+++ aexm/aexm_aux.v
@@ -1,4 +1,4 @@
-module cache_stall(input  cache_busy,
+module cache_stall(input  cache_busy_n,
 		   input  cpu_force,
 		   output cache_enable);
   reg 			  xcache_enable;
@@ -9,7 +9,7 @@ module cache_stall(input  cache_busy,
       if (cpu_force)
 	xcache_enable <= 1;
       else
-	if (cache_busy)
+	if (cache_busy_n)
 	  xcache_enable <= 1;
 	else
 	  xcache_enable <= 0;
