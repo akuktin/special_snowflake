6244ac66b60ba4a683b35c84515bcf51b644fba1
Aleksandar Kuktin <akuktin@gmail.com>
2016-04-09 14:50:46 +0200

    [aexm] Compiled aexm/cache_stall.

--- aexm/aexm_aux.v
+++ aexm/aexm_aux.v
@@ -8,7 +8,10 @@ module cache_stall(input  cache_busy_n,
   always @(cpu_force or cpu_req or cache_busy_n)
     begin
       case ({cpu_force,cpu_req,cache_busy_n})
-	3'b1xx: xcache_enable <= 1;
+	3'b100: xcache_enable <= 1;
+	3'b101: xcache_enable <= 1;
+	3'b110: xcache_enable <= 1;
+	3'b111: xcache_enable <= 1;
 	3'b011: xcache_enable <= 1;
 	default: xcache_enable <= 0;
       endcase // case ({cpu_force,cpu_req,cache_busy_n})
