b6fcfc9760f0413d1fee95fbf0490fc15b873090
Aleksandar Kuktin <akuktin@gmail.com>
2016-04-09 14:57:39 +0200

    [test] Added an empty test.

---
+++ test_base.v
@@ -0,0 +1,35 @@
+`timescale 1ns/1ps
+
+`include "test_inc.v"
+
+// Memory module
+`include "commands.v"
+`include "state.v"
+`include "initializer.v"
+`include "integration.v"
+
+// Cache
+`include "cpu_mcu.v"
+
+module GlaDOS;
+  reg CLK_p, CLK_n, CLK_dp, CLK_dn, RST, CPU_CLK;
+  reg [31:0] counter, minicounter, readcount, readcount2, readcount_r;
+
+  initial
+    forever
+      begin
+        #1.5 CLK_n <= 0; CLK_p <= 1;
+        #1.5 CLK_dp <= 1; CLK_dn <= 0;
+        #1.5 CLK_n <= 1; CLK_p <= 0;
+        #1.5 CLK_dp <= 0; CLK_dn <= 1;
+      end
+  initial
+    forever
+      begin
+        #1.5;
+        #4.5 CPU_CLK <= 1;
+        #3   CPU_CLK <= 0;
+      end
+
+
+endmodule // GlaDOS
