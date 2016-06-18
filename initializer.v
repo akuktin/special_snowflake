da4f0aa76b48ad1eb6899ed1ee92c8b15b200db0
Aleksandar Kuktin <akuktin@gmail.com>
2016-06-19 00:27:38 +0200

    [ddr] A workaround to the synthesizer.

--- initializer.v
+++ initializer.v
@@ -1,3 +1,12 @@
+`define NOOP 3'b111 /* no operation */
+`define ACTV 3'b011 /* activate (row open) */
+`define READ 3'b101 /* read */
+`define WRTE 3'b100 /* write */
+`define BTRM 3'b110 /* burst terminate */
+`define PRCH 3'b010 /* precharge (row close) */
+`define ARSR 3'b001 /* auto refresh/self refresh */
+`define MRST 3'b000 /* mode register set */
+
 module initializer(input CLK_n,
 		   input 	 RST,
 		   /* ----------------------- */
