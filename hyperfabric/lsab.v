01f57c4b899336edf222453097f708f8da252e1e
Aleksandar Kuktin <akuktin@gmail.com>
2016-07-15 01:05:29 +0200

    [hyper] Made lsab_cw output timely `I'm becoming full' signals.

--- hyperfabric/lsab.v
+++ hyperfabric/lsab.v
@@ -234,16 +234,20 @@ module lsab_cr(input CLK,
 endmodule // lsab_cr
 
 module lsab_cw(input CLK,
-              input             RST,
-              input             READ,
-              input             WRITE,
-              input [1:0]       READ_FIFO,
-              input [1:0]       WRITE_FIFO,
-              input [31:0]      IN,
+              input 		RST,
+              input 		READ,
+              input 		WRITE,
+              input [1:0] 	READ_FIFO,
+              input [1:0] 	WRITE_FIFO,
+              input [31:0] 	IN,
               output reg [31:0] OUT_0,
               output reg [31:0] OUT_1,
               output reg [31:0] OUT_2,
-              output reg [31:0] OUT_3);
+              output reg [31:0] OUT_3,
+	      output reg 	BFULL_0,
+	      output reg 	BFULL_1,
+	      output reg 	BFULL_2,
+	      output reg 	BFULL_3);
   reg               empty_0, empty_1, empty_2, empty_3;
   reg               full_0, full_1, full_2, full_3;
   reg [5:0]         len_0, len_1, len_2, len_3;
@@ -263,6 +267,8 @@ module lsab_cw(input CLK,
                     become_empty_2, become_empty_3;
   reg               become_full_0, become_full_1,
                     become_full_2, become_full_3;
+  reg               become_BFULL_0, become_BFULL_1,
+                    become_BFULL_2, become_BFULL_3;
   reg [5:0]         become_len_0, become_len_1,
                     become_len_2, become_len_3;
 
@@ -292,38 +298,106 @@ module lsab_cw(input CLK,
 
   always @(len_0 or do_read_0 or do_write_0 or full_0 or empty_0)
     case ({len_0,do_read_0,do_write_0})
-      {6'h3f,2'b10}: begin become_full_0 <= 0; become_empty_0 <= 0; end
-      {6'h3e,2'b01}: begin become_full_0 <= 1; become_empty_0 <= 0; end
-      {6'h01,2'b10}: begin become_full_0 <= 0; become_empty_0 <= 1; end
-      {6'h00,2'b01}: begin become_full_0 <= 0; become_empty_0 <= 0; end
-      default: begin become_full_0 <= full_0; become_empty_0 <= empty_0; end
+      {6'h3f,2'b10}: begin
+	become_full_0 <= 0; become_empty_0 <= 0; become_BFULL_0 <= 1;
+      end
+      {6'h3e,2'b01}: begin
+	become_full_0 <= 1; become_empty_0 <= 0; become_BFULL_0 <= 1;
+      end
+      {6'h38,2'b10}: begin
+	become_full_0 <= 0; become_empty_0 <= 0; become_BFULL_0 <= 0;
+      end
+      {6'h37,2'b01}: begin
+	become_full_0 <= 0; become_empty_0 <= 0; become_BFULL_0 <= 1;
+      end
+      {6'h01,2'b10}: begin
+	become_full_0 <= 0; become_empty_0 <= 1; become_BFULL_0 <= 0;
+      end
+      {6'h00,2'b01}: begin
+	become_full_0 <= 0; become_empty_0 <= 0; become_BFULL_0 <= 0;
+      end
+      default: begin
+	become_full_0 <= full_0; become_empty_0 <= empty_0;
+	become_BFULL_0 <= BFULL_0;
+      end
     endcase // case ({len_0,do_read_0,do_write_0})
 
   always @(len_1 or do_read_1 or do_write_1 or full_1 or empty_1)
     case ({len_1,do_read_1,do_write_1})
-      {6'h3f,2'b10}: begin become_full_1 <= 0; become_empty_1 <= 0; end
-      {6'h3e,2'b01}: begin become_full_1 <= 1; become_empty_1 <= 0; end
-      {6'h01,2'b10}: begin become_full_1 <= 0; become_empty_1 <= 1; end
-      {6'h00,2'b01}: begin become_full_1 <= 0; become_empty_1 <= 0; end
-      default: begin become_full_1 <= full_1; become_empty_1 <= empty_1; end
+      {6'h3f,2'b10}: begin
+	become_full_1 <= 0; become_empty_1 <= 0; become_BFULL_1 <= 1;
+      end
+      {6'h3e,2'b01}: begin
+	become_full_1 <= 1; become_empty_1 <= 0; become_BFULL_1 <= 1;
+      end
+      {6'h38,2'b10}: begin
+	become_full_1 <= 0; become_empty_1 <= 0; become_BFULL_1 <= 0;
+      end
+      {6'h37,2'b01}: begin
+	become_full_1 <= 0; become_empty_1 <= 0; become_BFULL_1 <= 1;
+      end
+      {6'h01,2'b10}: begin
+	become_full_1 <= 0; become_empty_1 <= 1; become_BFULL_1 <= 0;
+      end
+      {6'h00,2'b01}: begin
+	become_full_1 <= 0; become_empty_1 <= 0; become_BFULL_1 <= 0;
+      end
+      default: begin
+	become_full_1 <= full_1; become_empty_1 <= empty_1;
+	become_BFULL_1 <= BFULL_1;
+      end
     endcase // case ({len_1,do_read_1,do_write_1})
 
   always @(len_2 or do_read_2 or do_write_2 or full_2 or empty_2)
     case ({len_2,do_read_2,do_write_2})
-      {6'h3f,2'b10}: begin become_full_2 <= 0; become_empty_2 <= 0; end
-      {6'h3e,2'b01}: begin become_full_2 <= 1; become_empty_2 <= 0; end
-      {6'h01,2'b10}: begin become_full_2 <= 0; become_empty_2 <= 1; end
-      {6'h00,2'b01}: begin become_full_2 <= 0; become_empty_2 <= 0; end
-      default: begin become_full_2 <= full_2; become_empty_2 <= empty_2; end
+      {6'h3f,2'b10}: begin
+	become_full_2 <= 0; become_empty_2 <= 0; become_BFULL_2 <= 1;
+      end
+      {6'h3e,2'b01}: begin
+	become_full_2 <= 1; become_empty_2 <= 0; become_BFULL_2 <= 1;
+      end
+      {6'h38,2'b10}: begin
+	become_full_2 <= 0; become_empty_2 <= 0; become_BFULL_2 <= 0;
+      end
+      {6'h37,2'b01}: begin
+	become_full_2 <= 0; become_empty_2 <= 0; become_BFULL_2 <= 1;
+      end
+      {6'h01,2'b10}: begin
+	become_full_2 <= 0; become_empty_2 <= 1; become_BFULL_2 <= 0;
+      end
+      {6'h00,2'b01}: begin
+	become_full_2 <= 0; become_empty_2 <= 0; become_BFULL_2 <= 0;
+      end
+      default: begin
+	become_full_2 <= full_2; become_empty_2 <= empty_2;
+	become_BFULL_2 <= BFULL_2;
+      end
     endcase // case ({len_2,do_read_2,do_write_2})
 
   always @(len_3 or do_read_3 or do_write_3 or full_3 or empty_3)
     case ({len_3,do_read_3,do_write_3})
-      {6'h3f,2'b10}: begin become_full_3 <= 0; become_empty_3 <= 0; end
-      {6'h3e,2'b01}: begin become_full_3 <= 1; become_empty_3 <= 0; end
-      {6'h01,2'b10}: begin become_full_3 <= 0; become_empty_3 <= 1; end
-      {6'h00,2'b01}: begin become_full_3 <= 0; become_empty_3 <= 0; end
-      default: begin become_full_3 <= full_3; become_empty_3 <= empty_3; end
+      {6'h3f,2'b10}: begin
+	become_full_3 <= 0; become_empty_3 <= 0; become_BFULL_3 <= 1;
+      end
+      {6'h3e,2'b01}: begin
+	become_full_3 <= 1; become_empty_3 <= 0; become_BFULL_3 <= 1;
+      end
+      {6'h38,2'b10}: begin
+	become_full_3 <= 0; become_empty_3 <= 0; become_BFULL_3 <= 0;
+      end
+      {6'h37,2'b01}: begin
+	become_full_3 <= 0; become_empty_3 <= 0; become_BFULL_3 <= 1;
+      end
+      {6'h01,2'b10}: begin
+	become_full_3 <= 0; become_empty_3 <= 1; become_BFULL_3 <= 0;
+      end
+      {6'h00,2'b01}: begin
+	become_full_3 <= 0; become_empty_3 <= 0; become_BFULL_3 <= 0;
+      end
+      default: begin
+	become_full_3 <= full_3; become_empty_3 <= empty_3;
+	become_BFULL_3 <= BFULL_3;
+      end
     endcase // case ({len_3,do_read_3,do_write_3})
 
   always @(do_read_0 or do_write_0 or len_0)
@@ -422,9 +496,15 @@ module lsab_cw(input CLK,
        read_addr_2 <= 0; read_addr_3 <= 0;
        OUT_0 <= 0; OUT_1 <= 0; OUT_2 <= 0; OUT_3 <= 0;
        re_prev <= 0; re_fifo_prev <= 0;
+	BFULL_0 <= 0; BFULL_1 <= 0; BFULL_2 <= 0; BFULL_3 <= 0;
       end
     else
       begin
+	BFULL_0 <= become_BFULL_0;
+	BFULL_1 <= become_BFULL_1;
+	BFULL_2 <= become_BFULL_2;
+	BFULL_3 <= become_BFULL_3;
+
        empty_0 <= become_empty_0;
        full_0 <= become_full_0;
        empty_1 <= become_empty_1;
