4fc241f7882e4429933a2d6e86b4a5f091b5f587
Aleksandar Kuktin <akuktin@gmail.com>
2017-09-10 17:31:55 +0200

    [WIP][hyper] Added the blockmover interface to the Gremlin

--- hyperfabric/soft_ctrl.v
+++ hyperfabric/soft_ctrl.v
@@ -10,12 +10,20 @@ module Gremlin(input CLK,
 	       output reg [31:0] OUT_CPU,
 		    // ---------------------
 
-	       output reg [15:0] output_0,
-	       output reg [15:0] output_1,
-	       output reg [15:0] output_2,
+	       output reg 	 MCU_REFRESH_STROBE,
+
+	       output [15:0] 	 output_0,
+	       output [15:0] 	 output_1,
+	       output [15:0] 	 output_2,
+	       output [15:0] 	 output_3,
 	       input [15:0] 	 input_0,
 	       input [15:0] 	 input_1,
-	       input [15:0] 	 input_2
+	       input [15:0] 	 input_2,
+	       input [15:0] 	 input_3,
+	       input 		 input_0_we,
+	       input 		 input_1_we,
+	       input 		 input_2_we,
+	       input 		 input_3_we
 
 /*		 / * begin BLOCK MOVER * /
 		output [11:0] 	  BLCK_START,
@@ -51,6 +59,19 @@ module Gremlin(input CLK,
   wire [15:0] accumulator_adder, instr;
   wire [7:0] ip_nxt, d_r_addr_sys, d_w_addr_sys;
   wire 	     sys_cpu_en, d_w_en_sys, d_r_en_sys, cur_carry;
+
+  reg [3:0]  big_carousel;
+  reg [7:0]  small_carousel;
+  reg [1:0]  refresh_req, refresh_ack;
+
+  wire 	     trg_gb_0, trg_gb_1, trg_mb, time_rfrs;
+
+  reg [15:0] output_reg[3:0], input_reg[3:0];
+
+  assign output_0 = output_reg[0];
+  assign output_1 = output_reg[1];
+  assign output_2 = output_reg[2];
+  assign output_3 = output_reg[3];
 
   iceram16 data_mem(.RDATA(d_r_data), // 16 out
 		    .RADDR(d_r_addr), // 8 in
@@ -172,17 +193,11 @@ module Gremlin(input CLK,
 
   always @(posedge CLK)
     if (!RST)
-      TIME_REG <= 0;
-    else
-      TIME_REG <= TIME_REG -1;
-
-  always @(posedge CLK)
-    if (!RST)
       begin
 	accumulator <= 0; memory_operand <= 0; add_carry <= 0;
 	save_carry <= 0; ip <= 0; input_reg <= 0; index <= 0;
 	output_0 <= 0; output_0 <= 1; output_2 <= 0;
-	instr_f <= 0; instr_o <= 0;
+	instr_f <= 0; instr_o <= 0; wrote_3_strobe <= 0;
       end
     else
       if (sys_cpu_en)
@@ -229,14 +244,17 @@ module Gremlin(input CLK,
 	    end
 
 	    4'h4: accumulator <= TIME_REG;
-	    4'h5: accumulator <= input_reg;
-	    4'h6: accumulator <= input_reg;
-	    4'h7: accumulator <= input_reg;
+	    4'h5: accumulator <= input_reg[instr_o[1:0]];
+//	    4'h6
+//	    4'h7
 
 //	    4'h8 // store
-	    4'h9: output_0 <= accumulator;
-	    4'ha: output_1 <= accumulator;
-	    4'hb: output_2 <= accumulator;
+	    4'h9: begin
+	      output_reg[instr_o[1:0]] <= accumulator;
+	      wrote_3_strobe <= !wrote_3_strobe;
+	    end
+//	    4'ha
+//	    4'hb
 
 	    4'hc: index <= accumulator[7:0];
 	    4'hd: accumulator <= accumulator & memory_operand;
@@ -244,5 +262,104 @@ module Gremlin(input CLK,
 	    4'hf: accumulator <= accumulator ^ memory_operand;
 	  endcase // case (instr_o[11:8])
 	end // if (sys_cpu_en)
+
+
+  assign small_carousel_reset = small_carousel == 8'hbf;
+  assign BLCK_ISSUE = issue_op[0] ^ issue_op[1];
+
+  always @(posedge CLK)
+    if (!RST)
+      begin
+	small_carousel <= 8'hc1; // Out of bounds.
+	big_carousel <= 4'h3; refresh_req <= 0; refresh_ack <= 0;
+	TIME_REG <= 0; MCU_REFRESH_STROBE <= 0; wrote_3_antistrobe <= 0;
+      end
+    else
+      begin
+	TIME_REG <= TIME_REG -1;
+
+	if (small_carousel_reset)
+	  begin
+	    small_carousel <= 0;
+	    big_carousel <= big_carousel +1;
+	  end
+	else
+	  small_carousel <= small_carousel +1;
+
+	if (trans_activate &&
+	    (trg_gb_0 || trg_gb_1 || (time_mb && small_carousel != 0)))
+	  begin
+	    trans_active <= 1;
+	    wrote_3_antistrobe <= wrote_3_strobe;
+
+	    BLCK_START <= output_reg[0][11:0]; // provisional
+	    BLCK_SECTION <= output_reg[2][1:0]; // something
+	    BLCK_COUNT_REG <= output_reg[2][14:2]; // maybe
+	    MCU_PAGE_ADDR <= {output_reg[1],
+			      output_reg[0][15:12]}; // provisional
+	    MCU_REQUEST_ALIGN <= {output_reg[2][15],
+				  ~output_reg[2][15]}; // actually supposed
+            // to be the top usable bit in the address
+	  end // if (trans_activate &&...
+
+	if (((MCU_REQUEST_ALIGN[0] && MCU_GRANT_ALIGN[0]) ||
+	     (MCU_REQUEST_ALIGN[1] && MCU_GRANT_ALIGN[1])) &&
+	    trans_active)
+	  issue_op[0] <= !issue_op[0];
+	// again, supposed to be out of the if block
+	issue_op[1] <= issue_op[0];
+
+	blck_working_prev <= BLCK_WORKING;
+	if (blck_working_prev && !BLCK_WORKING)
+	  begin
+	    MCU_REQUEST_ALIGN <= 0;
+	    trans_active <= 0;
+	  end
+
+        if (trg_gb_0 && time_rfrs)
+          refresh_req <= refresh_req +1;
+
+	if (refresh_ctr_mismatch &&
+	    ! trans_active)
+	  begin
+	    refresh_ack <= refresh_ack +1;
+	    MCU_REFRESH_STROBE <= !MCU_REFRESH_STROBE;
+	  end
+
+	if (input_0_we)
+	  input_reg[0] <= input_0;
+	if (input_1_we)
+	  input_reg[1] <= input_1;
+	if (input_2_we)
+	  input_reg[2] <= input_2;
+	if (input_3_we)
+	  input_reg[3] <= input_3;
+      end
+
+///////////////////////////////////////////////////////////////
+
+  assign refresh_ctr_mismatch = refresh_req != refresh_ack;
+  assign trans_activate = wrote_3_strobe ^ wrote_3_antistrobe;
+
+  // Up to a maximum of 2 simultaneous 1 Gbps transactions.
+  // Up to a maximum of 6 simultaneous 12.5 Mbps transactions.
+
+  assign trg_gb_0 = small_carousel == 8'h00;
+  assign trg_gb_1 = small_carousel == 8'h60;
+//  assign trg_mb   = small_carousel == 8'h02;
+
+  assign time_mb = (big_carousel == 4'h4) || (big_carousel == 4'h6) ||
+		   (big_carousel == 4'h8) || (big_carousel == 4'ha) ||
+		   (big_carousel == 4'hc) || (big_carousel == 4'he);
+
+  /* Having mb and rfrs on the same big_carousel cycle is simply not
+   * supported, at least on the gb_0 side of the cycle. */
+  assign time_rfrs = (big_carousel == 4'h1) || (big_carousel == 4'h3) ||
+		     (big_carousel == 4'h5) || (big_carousel == 4'h7) ||
+		     (big_carousel == 4'h9) || (big_carousel == 4'hb) ||
+		     (big_carousel == 4'hd) || (big_carousel == 4'hf);
+
+///////////////////////////////////////////////////////////////
+
 
 endmodule // Gremlin
