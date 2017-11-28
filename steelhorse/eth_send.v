d9ff32702b2d0e7182b03707d4f6c44fbe3f04f5
Aleksandar Kuktin <akuktin@gmail.com>
2017-11-28 17:43:58 +0100

    [eth] Convert the staging registers to a single delay register in crcloader.

--- steelhorse/eth_send.v
+++ steelhorse/eth_send.v
@@ -8,7 +8,7 @@ module data_crcload(input CLK,
 		     output OCTET,
 		     output NEXT,
 		     output CRCHALT,
-		     output [15:0] DELAYDATA,
+		     output reg [31:0] DELAYDATA,
 		     output CRCBIT,
 		     output WRITE_CRCBITS,
                      output reg READ);
@@ -18,14 +18,12 @@ module data_crcload(input CLK,
   reg [10:0]		lenreg;
   reg [8:0]		addrreg;
   reg [7:0]		crcbits;
-  reg [15:0]		stagingreg1, stagingreg2;
 
   wire [10:0]		computed_len;
 
   assign computed_len = lenreg +5;
   assign OCTET = r1 ^ counter[3];
   assign WRITE_CRCBITS = delayreg_fast & ~CRCHALT & DO_GO;
-  assign DELAYDATA = stagingreg2;
   assign ADDR = addrreg;
   assign CRCBIT = complement_counter[5] ? crcbits[0] : ~crcbits[0];
   assign NEXT = (lenreg == SENDLEN);
@@ -66,8 +64,6 @@ module data_crcload(input CLK,
 		delayreg_slow <= 1;
 
 		crcbits <= DATA[23:16];
-		stagingreg1 <= DATA[31:16];
-		stagingreg2 <= stagingreg1;
 		READ <= 0;
 	      end
 	      2'h2: begin
@@ -76,9 +72,8 @@ module data_crcload(input CLK,
 	      end
 	      2'h3: begin
 		crcbits <= DATA[7:0];
-		stagingreg1 <= DATA[15:0];
-		stagingreg2 <= stagingreg1;
 		addrreg <= addrreg +1;
+		DELAYDATA <= DATA;
 		READ <= 1;
 	      end
 	    endcase // case (counter[4:3])
@@ -103,76 +98,71 @@ module data_encload(input CLK,
 		    input END_DATA,
 		    input OCTET,
 		    input [31:0] CRC,
-		    input [15:0] DATA,
+		    input [31:0] DATA,
 		    output BIT,
 		    output reg ENDOFSHOW);
   reg [7:0]		sendreg;
   reg [1:0] 		preamble_counter, crc_counter, data_counter;
-  reg			high, endisnigh;
+  reg			endisnigh;
+
+  wire [31:0] 		sendreg_data;
 
   assign BIT = sendreg[0];
 
   assign preamble = preamble_counter != 2'h3;
+  assign sendreg_data = preamble ? 32'hd5_55_55_55 :
+			END_DATA ? {CRC[24],CRC[25],CRC[26],CRC[27],
+				    CRC[28],CRC[29],CRC[30],CRC[31],
+
+				    CRC[16],CRC[17],CRC[18],CRC[19],
+				    CRC[20],CRC[21],CRC[22],CRC[23],
+
+				    CRC[8], CRC[9], CRC[10],CRC[11],
+				    CRC[12],CRC[13],CRC[14],CRC[15],
+
+				    CRC[0], CRC[1], CRC[2], CRC[3],
+				    CRC[4], CRC[5], CRC[6], CRC[7]} :
+			DATA;
+
 
   always @(posedge CLK)
     if (!RST)
       begin
-	high <= 1;
 	endisnigh <= 0;
 	sendreg <= 8'h55; /* preamble, part 1 */
 	ENDOFSHOW <= 0;
 
 	preamble_counter <= 0;
 	crc_counter <= 0;
-	data_conter <= 0;
+	data_conter <= 2'h1; // notice the dislocation
       end
     else
       if (DO_GO)
       begin
 	if (OCTET)
 	  begin
+	    data_counter <= data_counter +1;
+	    case (data_counter)
+	      2'h0: sendreg <= sendreg_data[31:24];
+	      2'h1: sendreg <= sendreg_data[23:16];
+	      2'h2: sendreg <= sendreg_data[15:8];
+	      2'h3: sendreg <= sendreg_data[7:0];
+	    endcase // case (data_counter)
+
 	    if (preamble)
 	      begin
 		preamble_counter <= preamble_counter +1;
-		case (preamble_counter)
-		  2'h0: sendreg <= 8'h55;
-		  2'h1: sendreg <= 8'h55;
-		  2'h2: sendreg <= 8'hd5;
-		endcase
 	      end
 
 	    if (END_DATA)
 	      begin
 		crc_counter <= crc_counter +1;
-		case (crc_counter)
-		  2'h0:
-		    sendreg <= {CRC[24],CRC[25],CRC[26],CRC[27],
-				CRC[28],CRC[29],CRC[30],CRC[31]};
-		  2'h1:
-		    sendreg <= {CRC[16],CRC[17],CRC[18],CRC[19],
-				CRC[20],CRC[21],CRC[22],CRC[23]};
-		  2'h2:
-		    sendreg <= {CRC[8], CRC[9], CRC[10],CRC[11],
-				CRC[12],CRC[13],CRC[14],CRC[15]};
-		  2'h3: begin
-		    sendreg <= {CRC[0], CRC[1], CRC[2], CRC[3],
-				CRC[4], CRC[5], CRC[6], CRC[7]};
-		    endisnigh <= 1;
-		  end
-		endcase // case (crc_counter)
+		if (crc_counter == 2'h3)
+		  endisnigh <= 1;
 	      end // if (END_DATA)
 
 	    if ((crc_counter == 2'h3) && endisnigh)
 	      ENDOFSHOW <= 1;
-
-	    if (!preamble && !END_DATA)
-	      begin
-		high <= !high;
-		if (high)
-		  sendreg <= DATA[15:8];
-		else
-		  sendreg <= DATA[7:0];
-	      end
 	  end // if (OCTET)
 	else
 	  sendreg <= {1'b0,sendreg[7:1]};
@@ -254,7 +244,7 @@ module send_integration(input CLK,
 			input [31:0] CRC_BUFF);
   wire			run_enc, octet, end_data;
   wire			bit_interconnect, endofshow;
-  wire [15:0]		data_interconnect;
+  wire [31:0]		data_interconnect;
 
   assign CRC_RST = RST;
 
