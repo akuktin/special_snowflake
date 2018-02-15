c39df6e4b5ddd8ebbb9013a01c5eea9477874b6e
Aleksandar Kuktin <akuktin@gmail.com>
2018-02-15 21:27:41 +0100

    [test] Update TT issue commands.

--- test/test_cmd.sh
+++ test/test_cmd.sh
@@ -1,1 +1,8 @@
-tt=03; for i in a b c d e f; do sed -i "s/\(CHANGER 8'h5\)./\1${i}/" test_tt_${tt}.bin; { iverilog test_special_snowflake_core2.v && vvp a.out; rm a.out; } 2>&1 | tee -a tt${tt}__${i}.test; echo DONE $i; done
+#1/bin/bash
+tt=00;
+ser=00;
+for i in a b c d e f; do
+  sed -i "s/\(CHANGER 8'h5\)./\1${i}/" test_tt_${tt}.bin;
+  { iverilog test_special_snowflake_core2.v && vvp a.out;
+    rm a.out; } 2>&1 | tee -a tt${tt}__${i}.${ser}.log;
+done
--- test/test_cmd_stall.sh
+++ test/test_cmd_stall.sh
@@ -1,1 +1,9 @@
-tt=interrupt_stall; for i in 0b 0c 0d 0e 0f 10 11 12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f 20 21 22 23 24 25; do sed -i "s/\(CHANGER 64'h\)../\1${i}/" test_${tt}.bin; { iverilog test_special_snowflake_core2.v && vvp a.out; rm a.out; } 2>&1 | tee -a tt${tt}__${i}.test; done
+#!/bin/bash
+tt=interrupt_stall;
+ser=00;
+for i in 0b 0c 0d 0e 0f 10 11 12 13 14 15 16 17 18
+         19 1a 1b 1c 1d 1e 1f 20 21 22 23 24 25; do
+  sed -i "s/\(CHANGER 64'h\)../\1${i}/" test_${tt}.bin;
+  { iverilog test_special_snowflake_core2.v && vvp a.out;
+    rm a.out; } 2>&1 | tee -a tt${tt}__${i}.${ser}.log;
+done
